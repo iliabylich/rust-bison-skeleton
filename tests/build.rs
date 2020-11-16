56394b623c93106b60ce39cfcd82e492dfaaf1b6
Ilya Bylich <ibylich@gmail.com>
2020-11-16 17:37:04 +0300

    rustfmt

--- tests/build.rs
+++ tests/build.rs
@@ -4,7 +4,7 @@ use std::path::Path;
 
 fn main() {
     match process_bison_file(&Path::new("src/calc.y")) {
-        Ok(_) => {},
+        Ok(_) => {}
         Err(BisonErr { message, .. }) => {
             eprintln!("Bison error:\n{}\nexiting with 1", message);
             std::process::exit(1);
